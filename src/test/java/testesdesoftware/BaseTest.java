f7cba8ad98ba2b8121f90298b7c15bd89a0b2f41
Rodrigo Barreto <114615381+RodrigoBartz@users.noreply.github.com>
2023-11-22 19:03:52 -0300

    Add files via upload

---
+++ src/test/java/testesdesoftware/BaseTest.java
@@ -0,0 +1,26 @@
+package testesdesoftware;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+	protected static WebDriver driver;
+	private static final String URL_BASE = "https://www.google.com";
+	private static final String PATH_DRIVE = "src/test/resources/chromedriver11906045.exe";
+	
+	@BeforeClass
+	public static void iniciar() {
+		
+		System.setProperty("webdriver.chrome.driver", PATH_DRIVE);
+		driver = new ChromeDriver(); //só abre navegador
+		driver.manage().window().maximize(); //maximiza a janela do navegador
+		driver.get(URL_BASE); // injeta um endereço no navegador e faz request
+	}
+	
+	@AfterClass
+	public static void finalizar() {
+		driver.quit();
+	}
+}
---
+++ src/test/java/testesdesoftware/GoogleRefatoradoTest.java
@@ -0,0 +1,27 @@
+package testesdesoftware;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class GoogleRefatoradoTest extends BaseTest{
+
+	@Test
+	public void pesquisarNoGoogle() {
+		
+		//preparação
+		WebElement inputPesquisa = driver.findElement(By.name("q"));
+		
+		//execução
+		inputPesquisa.sendKeys("bolo de laranja" + Keys.ENTER);
+		
+		String resultado = driver.findElement(By.id("result-stats")).getText();
+		
+		//verificação e análise
+		assertTrue(resultado, resultado.contains("Aproximadamente"));
+		
+	}
+}
